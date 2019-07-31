57c2280655d773fe000e8b1a4f4f46eefaae0b8a
Curso Git <marcooperdomo@gmail.com>
2019-07-30 23:44:27 -0500

    Elementos a La Lista | Directivas, métodos

--- WorkshopVueJs2/script.js
+++ WorkshopVueJs2/script.js
@@ -2,9 +2,16 @@ new Vue({
   el: '#appVue',
   data: {
     lists: [
-      {keep: 'uno', completed: true},
-      {keep: 'dos', completed: false},
-      {keep: 'tres', completed: true},
+      // {keep: 'uno', completed: true},
+      // {keep: 'dos', completed: false},
+      // {keep: 'tres', completed: true},
     ],
+    newKeep: '',
+  },
+  methods: {
+    addKeep: function(){
+      this.lists.push({keep: this.newKeep, completed: false});
+      this.newKeep = '';
+    }
   }
 });
